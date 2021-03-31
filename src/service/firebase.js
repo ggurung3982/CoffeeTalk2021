36f50e4f0133437029916beee5def5edb6506624
gita gurung <gurunggita12@gmail.com>
2021-03-31 01:07:11 -0400

    remove try catch from for fb initialize

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -2,15 +2,16 @@ import firebase from 'firebase/app'; // <-- This must be first
 import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/firestore';
-import config from './config'
+import config from './config';
 
-try {
-  firebase.initializeApp(config);
-} catch (error) {
-  if (!/already exists/u.test(error.message)) {
-    console.error('Firebase admin initialization error', error.stack);
-  }
-}
+firebase.initializeApp(config);
+// try {
+//   firebase.initializeApp(config);
+// } catch (error) {
+//   if (!/already exists/u.test(error.message)) {
+//     console.error('Firebase admin initialization error', error.stack);
+//   }
+// }
 
 // Passing off firebase.auth() instead of firebase.auth
 // allows us to share the same instance of Firebase throughout
